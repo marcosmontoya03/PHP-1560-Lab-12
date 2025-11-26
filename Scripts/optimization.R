9e8087183ccf74b0d4a43f1df8569b03f5a3e1d7
Gavin Schilling <gavin_schilling@brown.edu>
2025-11-26 18:01:39 +0000

    Creating a way to add happiness and move bikes
    what the title says

--- Scripts/optimization.R
+++ Scripts/optimization.R
@@ -4,12 +4,54 @@ library(dplyr)
 library(tidyverse)
 library(lubridate)
 
+source("estimation.R")
+source("modeling.R")
+
+simulate_bikes(df_simulation)
+
+#' Function to test the number of happy customers
+#' 
+#' @param df_sim A data frame with time, starting station and ending station 
+#' @param placement A starting placement of the bikes, should be a dataframe of
+#' stations and the number of bikes in them
+simulate_bikes <- function(df_sim, df_place){
+  
+  #Makes a empty placement of bikes if no placement is inputted
+  if(is.null(df_place)){
+    df_place <- data.frame(station = seq(1,24,1), bikes = rep(0,24))
+  }
+  
+  #Loops through the simulated trips dataframe, moving a bike if one is available,
+  # and creating a happiness output
+  for(i in 1:length(df_sim)){
+    start <- df_sim$start_station[i]
+    end <- df_sim$end_station[i]
+    
+    
+    if(df_place[start]$num_bikes >= 1){
+      df_place[start]$num_bikes <-  df_place[start]$num_bikes - 1
+      df_place[end]$num_bikes <- df_place[end]$num_bikes + 1
+      df_sim[i]$feelings <- "happy"
+    }
+    else{
+      df_sim[i]$feelings <- "unhappy"
+    }
+  }
+}
+
+# A function to simulate several days, and using simulate_bikes each time
+# where an input is the number of days you want to simulate
+
+
+
+
+
 optimize <- function(num_bikes, df_rates){
-  start_place <- #Make this shit random
+  start_place <- #Make this random
   happy_score <- c()
     
   for(i in 1:5){  
-  modeling_output <- source("modeling.R") #See the other script for this function
+  modeling_output <-  #See the other script for this function
   
   #' Will take in the starting bike placement and compare it with the modeled bike
   #' usage
